4ca246b5860be5da0cb6211d2ffa2b34b8c54db2
Unti1 <kostyuchenko.work@gmail.com>
2023-09-04 22:40:33 +0300

    Правки

--- check_main.py
+++ check_main.py
@@ -9,5 +9,5 @@ for proc in all_proc:
     if "mail_main" in proc:
         finding_proc = True
 
-if finding_proc:
+if finding_proc == False:
     os.system("nohup source Bot/MAIL_BOT/bin/activate && python mail_main.py")

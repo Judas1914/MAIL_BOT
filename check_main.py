eb7a09cff37e3ff414e0a6c3f904246ac773ebe7
Judas1914 <gleb1234abcd@gmail.com>
2023-10-25 23:21:38 +0300

    Changes to be committed: 	modified:   check_main.py

--- check_main.py
+++ check_main.py
@@ -12,9 +12,12 @@ python_executable = PATH + "/bin/python3"
 
 # Получить список всех процессов Python
 def is_process_running(process_name):
+    bot.send_message(config['meid']['id'], "Bot verification")
     for proc in psutil.process_iter(["pid", "name"]):
         if process_name in proc.info["name"]:
+            bot.send_message(config['meid']['id'], "The bot is working")
             return True
+    bot.send_message(config['meid']['id'], "Bot doesn't work")
     return False
 
 if not is_process_running(process_name):
